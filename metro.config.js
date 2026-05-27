86e771331cef59f9d735dcd6e3df12ba0f1d802c
Cris <cristian.cioanca@gmail.com>
2026-05-26 22:11:49 -0400

    More fun package stuff + metro config

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,8 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+if (!config.resolver.assetExts.includes('wasm')) {
+	config.resolver.assetExts.push('wasm');
+}
+
 module.exports = withNativeWind(config, { input: './global.css' });
